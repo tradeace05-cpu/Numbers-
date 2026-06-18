6079232a185dfc872a552bc64463d6870070c2d7
tradeace05-cpu <tradeace05@gmail.com>
2026-06-18 13:10:40 +0100

    Update buy-number.js

--- netlify/functions/buy-number.js
+++ netlify/functions/buy-number.js
@@ -2,8 +2,18 @@ exports.handler = async (event) => {
   try {
     const { service, country } = JSON.parse(event.body);
     
-    const response = await fetch(`https://oraclelense.com/api/v1/buy?api_key=${process.env.ORACLE_API_KEY}&product_id=${service}&server=${country}&quantity=1`, {
-      method: 'GET'
+    const formData = new URLSearchParams();
+    formData.append('api_key', process.env.ORACLE_API_KEY);
+    formData.append('product_id', service);
+    formData.append('server', country);
+    formData.append('quantity', '1');
+    
+    const response = await fetch('https://oraclelense.com/api/v1/buy', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: formData
     });
     
     const data = await response.json();
