c92546a2a30f608b138e15f520e0e640774d9565
tradeace05-cpu <tradeace05@gmail.com>
2026-06-18 14:02:34 +0100

    Update buy-number.js

--- netlify/functions/buy-number.js
+++ netlify/functions/buy-number.js
@@ -1,19 +1,19 @@
 exports.handler = async (event) => {
   try {
     const { service, country } = JSON.parse(event.body);
-    
+
     const response = await fetch(`https://5sim.net/v1/user/buy/activation/${country}/${service}`, {
       method: 'GET',
       headers: {
         'api-key': process.env.FIVESIM_API_KEY
       }
     });
-    
+
     const data = await response.json();
-    
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ 
+      body: JSON.stringify({
         number: data.phone || data.number || 'No number',
         orderId: data.id,
         full: data
